8a43dfbf708753384a859ccc5e879a9fc2ed2d75
iriojose <iriojgomezv@gmail.com>
2024-09-17 23:55:54 -0400

    set headers

--- src/main.ts
+++ src/main.ts
@@ -5,20 +5,22 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
+    app.enableCors({
+        origin: "*",
+        methods: ["GET", "POST", "PUT"],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+		credentials: true,
+    })
+
     const options = new DocumentBuilder()
-      .setTitle('Poke App')
-      .setDescription('Api for return pokemons informations')
-      .setVersion('1.0')
-      .addServer('http://localhost:3000/', 'Local environment')
-      .build();
+        .setTitle('Poke App')
+        .setDescription('Api for return pokemons informations')
+        .setVersion('1.0')
+        .addServer('http://localhost:3000/', 'Local environment')
+        .build();
   
     const document = SwaggerModule.createDocument(app, options);
     SwaggerModule.setup('api-docs', app, document);
-
-    app.enableCors({
-      origin: "*",
-      methods: ["GET", "POST", "PUT"]
-    })
 
     await app.listen(3000);
 }
